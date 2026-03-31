3b80b5c1b22f8312ff4a94c75c14b72a2c623193
suryavamsi360 <suryavamsi360@gmail.com>
2026-03-31 11:19:15 +0530

    default

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -24,6 +24,7 @@ export default function ThankYouPage() {
       if (clientName) {
         window.location.href = `https://${clientName}.ambitionhire.ai/candidate/dashboard`;
       } else {
+        window.location.href = `https://services.ambitionhire.ai/candidate/dashboard`;
         console.error("Client name not found in localStorage");
       }
     }, 5000);
